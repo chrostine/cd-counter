aa66286bec1b1547ec4e5db365c828d7df94c515
chrostine <christineswierkosz@gmail.com>
2026-03-09 14:28:08 +0100

    added work

---
+++ sketch.js
@@ -0,0 +1,32 @@
+let textFarve = "#000000"
+let button
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  currentNumber = getRandomElement(talArray);
+  
+  // opret knap der kalder getRandomElement(arr)
+  button = createButton('generer nyt tal');
+  button.position(width/2, height/2+50);
+  button.center('horizontal');
+  button.mousePressed(() => {
+    currentNumber = getRandomElement(talArray); // opdater currentNumber ved klik
+    // kontrol
+  console.log("Tilfældigt tal:", currentNumber, "Længde på array:", talArray.length); 
+  });
+}
+
+function draw() {
+  background(220);
+
+  // visning af nummer
+  textAlign(CENTER, CENTER);
+  textSize(32);
+  fill(textFarve);
+  text(currentNumber  ?? "hvad sker der", width/2, height/2)
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
